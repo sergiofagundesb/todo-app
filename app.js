620023f12227219ba9e57478bb45806a49db5304
sergiofagundesb <sergio.fagundesbarros@gmail.com>
2021-04-22 08:36:51 -0300

    populate

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
-const bodyParser = require('body-parser')
-const port = 3001
+const usuarioController = require('./src/controllers/usuario-controller');
+const tarefaController = require ('./src/controllers/tarefa-controller');
+const dados = require('./infra/bd')
+const port = 3000
 
-app.use(bodyParser.json())
-
-const usuario = require('./src/controllers/usuario-controller')
-const tarefa = require('./src/controllers/tarefa-controller')
-
-const db = require('./infra/bd')
-
-usuario(app, db)
-tarefa(app, db)
-
-app.listen(port, () => {
-    console.log(`Ouvindo em http://localhost:${port}`)
-})
+app.use(bodyParser.json());
+usuarioController(app, dados);
+tarefaController(app, dados);
+app.listen(port, () => console.log (`Servidor rodando no localhost:${port}`));
